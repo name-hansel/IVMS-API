d6941c4104859eec54e68cf355fb31084ee7b31c
snehal-mane <snehal6mane@gmail.com>
2020-09-15 19:43:07 +0530

    added new route for company to add new tour

---
+++ API/tour/postNewTour.php
@@ -0,0 +1,36 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Tour.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$tour = new Tour($db);
+//decode posted data
+$data = json_decode(file_get_contents("php://input"));
+
+$tour->name = $data->name;
+$tour->branch = $data->branch;
+$tour->company_id = $data->company_id;
+$tour->available_days = $data->available_days;
+$tour->place = $data->place;
+$tour->number_people = $data->number_people;
+$tour->number_days = $data->number_days;
+$tour->rate = $data->rate;
+$tour->description = $data->description;
+
+if ($tour->postNewTour()) {
+    echo json_encode(
+        array('message' => 'Tour Created')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Tour Not Created')
+    );
+}
--- models/Tour.php
+++ models/Tour.php
@@ -1,30 +1,53 @@
 <?php
-    class Tour {
-        private $conn;
-        private $table = 'tour';
+class Tour
+{
+    private $conn;
+    private $table = 'tour';
 
-        public $tour_id;
-        public $name;
-        public $branch;
-        public $company_id;
-        public $available_days;
-        public $place;
-        public $number_people;
-        public $number_days;
-        public $rate;
-        public $description;
-        public $avg_rating;
+    public $tour_id;
+    public $name;
+    public $branch;
+    public $company_id;
+    public $available_days;
+    public $place;
+    public $number_people;
+    public $number_days;
+    public $rate;
+    public $description;
+    public $avg_rating;
 
-        public function __construct($db) {
-            $this->conn = $db;
-        }
-
-        public function getSampleTourData() {
-            $query = 'SELECT tour_id, name, branch, place, rate FROM '.$this->table. ' WHERE company_id = ? LIMIT 4';
-            $stmt = $this->conn->prepare($query);
-            $stmt->bindParam(1, $this->company_id);
-            $stmt->execute();
-
-            return $stmt;
-        }
+    public function __construct($db)
+    {
+        $this->conn = $db;
     }
+
+    public function getSampleTourData()
+    {
+        $query = 'SELECT tour_id, name, branch, place, rate FROM ' . $this->table . ' WHERE company_id = ? LIMIT 4';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->company_id);
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function postNewTour()
+    {
+        $d = "15-09-2020";
+        $query = "INSERT INTO tour(name, branch, company_id, available_days, place, number_people, number_days, rate, description) VALUES (?,?,?,?,?,?,?,?,?)";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(1, $this->name);
+        $stmt->bindParam(2, $this->branch);
+        $stmt->bindParam(3, $this->company_id);
+        $stmt->bindParam(4, $d);
+        $stmt->bindParam(5, $this->place);
+        $stmt->bindParam(6, $this->number_people);
+        $stmt->bindParam(7, $this->number_days);
+        $stmt->bindParam(8, $this->rate);
+        $stmt->bindParam(9, $this->description);
+
+        $stmt->execute();
+        return $stmt;
+    }
+}
