55e97074cffdf9fb573b4793e228926952651156
snehal-mane <snehal6mane@gmail.com>
2020-11-04 22:02:41 +0530

    added getCompanyPastTours route

---
+++ API/bookedTour/getCompanyPastTours.php
@@ -0,0 +1,41 @@
+<?php
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/BookedTour.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$bookedTour = new BookedTour($db);
+$bookedTour->company_id = 1;
+
+$pastTourResult = $bookedTour->getCompanyPastTours();
+$pastTourNum = $pastTourResult->rowCount();
+$pastTourArray = array();
+$pastTourArray['data'] = array();
+
+if ($pastTourNum > 0) {
+    while ($row = $pastTourResult->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $pastTour_item = array(
+            'btour_id' => $btour_id,
+            'tour_id' => $tour_id,
+            'user_id' => $user_id,
+            'name' => $name,
+            'college' => $college,
+            'date' => $available_days,
+            'booked_at' => $booked_at,
+            'number_people' => $number_people,
+            'rating' => $rating
+        );
+        array_push($pastTourArray['data'], $pastTour_item);
+    }
+} else {
+    $message = array('message' => 'No past tours found');
+    array_push($pastTourArray['data'], $message);
+}
+
+echo json_encode($pastTourArray);
--- models/BookedTour.php
+++ models/BookedTour.php
@@ -56,4 +56,19 @@ class BookedTour
 
         return $stmt;
     }
+
+    public function getCompanyPastTours()
+    {
+        $query = 'SELECT btour_id, btour.tour_id, btour.user_id, tour.name, coordinator.college, tour.available_days, btour.booked_at, btour.number_people, btour.rating FROM btour INNER JOIN tour 
+        ON tour.tour_id = btour.tour_id 
+        INNER JOIN coordinator
+        ON coordinator.user_id = btour.user_id
+        WHERE (tour.company_id = ? AND tour.available_days < CURRENT_DATE)';
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->company_id);
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
