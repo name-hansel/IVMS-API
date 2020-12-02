9146f478ea6f28076e9286b143401ab23ea1d084
name-hansel <snehal6mane@gmail.com>
2020-12-02 09:24:03 +0530

    minor changes

--- models/Company.php
+++ models/Company.php
@@ -27,7 +27,7 @@ class Company
 
     public function getHomeCompany()
     {
-        $query = 'SELECT company, description FROM company';
+        $query = 'SELECT company, description FROM company LIMIT 4';
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
--- models/Tour.php
+++ models/Tour.php
@@ -83,7 +83,7 @@ class Tour
 
     public function getHomeTour()
     {
-        $query = 'SELECT name,description FROM tour';
+        $query = 'SELECT name,description FROM tour LIMIT 4';
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
