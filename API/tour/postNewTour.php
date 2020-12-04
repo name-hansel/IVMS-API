90b4769eff5b88c860dd7b37a271362853db450c
name-hansel <snehal6mane@gmail.com>
2020-12-04 20:15:23 +0530

    changed origin

--- API/tour/postNewTour.php
+++ API/tour/postNewTour.php
@@ -1,6 +1,6 @@
 <?php
 // Headers
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://industrialvisit-ms.herokuapp.com');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
